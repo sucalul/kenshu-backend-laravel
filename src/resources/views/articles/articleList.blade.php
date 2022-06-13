c459fe8b264b038863d2440831b09f4e836b5395
yuya.miyazaki <cheeeese4869@gmail.com>
2022-06-13 16:13:06 +0900

    apply review

--- src/resources/views/articles/articleList.blade.php
+++ src/resources/views/articles/articleList.blade.php
@@ -6,12 +6,12 @@
 </head>
 <body>
 @foreach ($articles as $article)
-    <h3><?= htmlspecialchars($article['id'], ENT_QUOTES, "UTF-8") ?></h3>
-    <img src="/img/<?= htmlspecialchars($article['thumbnail_image_id'], ENT_QUOTES, "UTF-8") ?>.png" alt="" style="width:200px; height:200px">
-    <h3><?= htmlspecialchars($article['title'], ENT_QUOTES, "UTF-8") ?></h3>
-    <p>書いた人: <?= htmlspecialchars($article->user->name, ENT_QUOTES, "UTF-8") ?></p>
-    <button type="button" class="btn btn-info" onclick="location.href='/articles/<?= htmlspecialchars($article['id'], ENT_QUOTES, "UTF-8") ?>'">More</button>
-    <form action="/articles/<?= htmlspecialchars($article['id'], ENT_QUOTES, 'UTF-8') ?>/delete" method="post">
+    <h3>{{ $article['id']}}</h3>
+    <img src="/img/{{ $article['thumbnail_image_id'] }}.png" alt="" style="width:200px; height:200px">
+    <h3>{{ $article['title'] }}</h3>
+    <p>書いた人: {{ $article->user->name }}</p>
+    <button type="button" class="btn btn-info" onclick=location.href="/articles/{{ $article['id'] }}">More</button>
+    <form action="/articles/{{ $article['id'] }}/delete" method="post">
         <button type="submit">Delete</button>
     </form>
     <p>-------------------------</p>
