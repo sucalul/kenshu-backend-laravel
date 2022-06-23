0b0ef913cd6cfd9200232b754562ed3baeab35ae
yuya.miyazaki <cheeeese4869@gmail.com>
2022-06-23 17:21:54 +0900

    does not work ;

--- src/tests/Unit/Services/ThumbnailServiceTest.php
+++ src/tests/Unit/Services/ThumbnailServiceTest.php
@@ -6,17 +6,23 @@ use App\Services\ThumbnailService;
 use App\Http\Requests\CreateArticleRequest;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
 use Mockery;
+use Mockery\MockInterface;
+use Illuminate\Support\Facades\Storage;
+
 
 class ThumbnailServiceTest extends TestCase
 {
     use WithFaker;
 
     private CreateArticleRequest $request;
+    private ThumbnailService $thumbnailService;
 
     public function setUp(): void
     {
         parent::setUp();
+        $this->thumbnailService = app(ThumbnailService::class);
         $this->request = new CreateArticleRequest();
     }
 
@@ -29,5 +35,14 @@ class ThumbnailServiceTest extends TestCase
             $return_value,
             $mock->execute($this->request)
         );
+    }
+
+    public function testThumbnailExists()
+    {
+        Storage::fake('design');
+        $service = $this->thumbnailService;
+        $uploadedFile = UploadedFile::fake()->image('design.jpg');
+        $service->execute($uploadedFile);
+        Storage::disk('design')->assertExists($uploadedFile->getFilename());
     }
 }
